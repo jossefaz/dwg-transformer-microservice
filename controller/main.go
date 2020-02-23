cc7b2254123f25bf9247c7adfcf3b7e6056dd10c
yossefazoulay <azoulayos@protonmail.com>
2020-02-23 12:11:50 +0200

    utils.go created in controller, list all files in directory

--- controller/main.go
+++ controller/main.go
@@ -1,18 +1,29 @@
 package main
 
-import "time"
+import "fmt"
 
 func main() {
-	rmqConn := newRabbit("amqp://guest:guest@rabbitmq/", "transformDWG")
-	defer rmqConn.conn.Close()
-	defer rmqConn.chanL.Close()
-	message := pickFile{
-		Name: "test",
-		Path: "pathtest",
-	}
-	for i := 0; i < 1000; i++ {
-		time.Sleep(time.Second)
-		rmqConn.sendMessage(message)
+	// rmqConn := newRabbit("amqp://guest:guest@rabbitmq/", "transformDWG")
+	// defer rmqConn.conn.Close()
+	// defer rmqConn.chanL.Close()
+
+	// for i := 0; i < 1000; i++ {
+	// 	time.Sleep(time.Second)
+	// 	rmqConn.sendMessage(message)
+	// }
+
+	root := "./"
+
+	files := listFilesInDir(root)
+
+	for _, file := range files {
+
+		message := pickFile{
+			Name: "File Uploaded",
+			Path: file,
+		}
+		// rmqConn.sendMessage(message)
+		fmt.Println(message)
 	}
 
 }
---
+++ controller/utils.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func listFilesInDir(root string) []string {
+
+	files := []string{}
+
+	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+
+		fi, err := os.Stat(path)
+
+		if err != nil {
+			fmt.Println(err)
+			return nil
+		}
+		switch mode := fi.Mode(); {
+		case mode.IsDir():
+			// do directory stuff
+			fmt.Println("directory")
+		case mode.IsRegular():
+			// do file stuff
+			files = append(files, path)
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		panic(err)
+	}
+	return files
+}
