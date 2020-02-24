ef0b43b14ab992e4df6eef32d8f91209fb710070
yossefazoulay <azoulayos@protonmail.com>
2020-02-24 12:19:24 +0200

    combine producer and consumer into queue.go

---
+++ transformer/src/queue.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"os/exec"
+
+	"github.com/streadway/amqp"
+)
+
+type pickFile struct {
+	Name string
+	Path string
+}
+
+type rabbitmq struct {
+	conn  *amqp.Connection
+	chanL *amqp.Channel
+	queue amqp.Queue
+}
+
+func handleError(err error, msg string) {
+	if err != nil {
+		fmt.Printf("%s: %s", msg, err)
+	}
+
+}
+
+func newRabbit(connString string, queueName string) (instance rabbitmq) {
+	conn := dial(connString)
+	amqpChannel := getChannel(conn)
+	queue := connectToQueue(amqpChannel, queueName)
+	return rabbitmq{
+		conn:  conn,
+		chanL: amqpChannel,
+		queue: queue,
+	}
+}
+
+func dial(connString string) *amqp.Connection {
+	conn, err := amqp.Dial(connString)
+	handleError(err, "Can't connect to AMQP")
+	if err != nil {
+		os.Exit(1)
+	}
+	return conn
+}
+
+func getChannel(conn *amqp.Connection) *amqp.Channel {
+	c, err := conn.Channel()
+	handleError(err, "Can't create a amqpChannel")
+	if err != nil {
+		os.Exit(1)
+	}
+	return c
+}
+
+func connectToQueue(c *amqp.Channel, queueName string) amqp.Queue {
+	q, err := c.QueueDeclare(queueName, true, false, false, false, nil)
+	handleError(err, "Could not declare `add` queue")
+	if err != nil {
+		os.Exit(1)
+	}
+	return q
+}
+
+func (rmq rabbitmq) sendMessage(m pickFile) {
+	body, err := json.Marshal(m)
+	if err != nil {
+		handleError(err, "Error encoding JSON")
+	}
+	err = rmq.chanL.Publish("", rmq.queue.Name, false, false, amqp.Publishing{
+		DeliveryMode: amqp.Persistent,
+		ContentType:  "text/plain",
+		Body:         body,
+	})
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(m)
+}
+
+func (rmq rabbitmq) listenMessage() {
+
+	err := rmq.chanL.Qos(1, 0, false)
+	handleError(err, "Could not configure QoS")
+	messageChannel, err := rmq.chanL.Consume(
+		rmq.queue.Name,
+		"",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	handleError(err, "Could not register consumer")
+
+	stopChan := make(chan bool)
+
+	go func() {
+		counter := 0
+		fmt.Printf("Consumer ready, PID: %d", os.Getpid())
+		for d := range messageChannel {
+			counter++
+			fmt.Printf("\nReceived a message: %s \n", d.Body)
+
+			pFIle := &pickFile{}
+
+			err := json.Unmarshal(d.Body, pFIle)
+
+			if err != nil {
+				fmt.Printf("Error decoding JSON: %s", err)
+			}
+			if err := d.Ack(false); err != nil {
+				fmt.Printf("Error acknowledging message : %s", err)
+			} else {
+				outpath := pFIle.Path[:len(pFIle.Path)-3] + "dxf"
+				cmd := exec.Command("dwgread", pFIle.Path, "-O", "DXF", "-o", outpath)
+				out, err := cmd.CombinedOutput()
+				if err != nil {
+					fmt.Printf("cmd.Run() failed with %s\n", err)
+				}
+				fmt.Println(string(out))
+			}
+
+		}
+	}()
+
+	// Stop for program termination
+	<-stopChan
+
+}
