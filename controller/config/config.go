b6ca37dbade57b9f149d64a9ff0ed15ba5bfc5bd
yossefazoulay <azoulayos@protonmail.com>
2020-02-25 17:30:55 +0200

    fix Dockerfile for controller

--- controller/config/config.go
+++ controller/config/config.go
@@ -1,11 +1,9 @@
-package main
+package config
 
 import (
 	"github.com/tkanos/gonfig"
-<<<<<<< HEAD:controller/config/config.go
 	"github.com/yossefazoulay/go_utils/utils"
-=======
->>>>>>> 87629761a850af82eefc4851d27a76e5f8d416cd:lib/config/config.go
+
 )
 
 type Configuration struct {
@@ -26,7 +24,7 @@ func GetConfig(env string )  Configuration {
 	configEnv["prod"] = "./config/config.prod.json"
 	configuration := Configuration{}
 	err := gonfig.GetConf(configEnv[env], &configuration)
-	HandleError(err, "Cannot load/read config file")
+	utils.HandleError(err, "Cannot load/read config file")
 	return configuration
 }
 
