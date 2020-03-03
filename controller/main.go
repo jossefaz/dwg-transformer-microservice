299570b4a0bad40b889636efbafac1b931feb452
yossefazoulay <azoulayos@protonmail.com>
2020-03-03 11:16:40 +0200

    delete commeted code in controller

--- controller/main.go
+++ controller/main.go
@@ -2,18 +2,20 @@ package main
 
 import (
 	"controller/config"
+	"controller/utils"
+	"github.com/yossefazoulay/go_utils/queue"
+	globalUtils "github.com/yossefazoulay/go_utils/utils"
 	"os"
 )
 
 func main() {
 
 	config.GetConfig(os.Args[1])
-	config.Logger.Log.Info("Ceci est un test")
-	//queueConf := config.LocalConfig.Queue.Rabbitmq
-	//rmqConn, err := queue.NewRabbit(queueConf.ConnString, queueConf.QueueNames)
-	//globalUtils.HandleError(err, "Error Occured when RabbitMQ Init", config.Logger)
-	//defer rmqConn.Conn.Close()
-	//defer rmqConn.ChanL.Close()
-	//utils.MockData(rmqConn)
-	//rmqConn.OpenListening(queueConf.Listennig, utils.MessageReceiver)
+	queueConf := config.LocalConfig.Queue.Rabbitmq
+	rmqConn, err := queue.NewRabbit(queueConf.ConnString, queueConf.QueueNames)
+	globalUtils.HandleError(err, "Error Occured when RabbitMQ Init", config.Logger)
+	defer rmqConn.Conn.Close()
+	defer rmqConn.ChanL.Close()
+	utils.MockData(rmqConn)
+	rmqConn.OpenListening(queueConf.Listennig, utils.MessageReceiver)
 }
