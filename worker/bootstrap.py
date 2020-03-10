dd27e01767c041e91676b2c090abbb239abf07d0
yossefazoulay <azoulayos@protonmail.com>
2020-03-10 12:31:03 +0200

    check if input file ags is json and convert if not

--- worker/bootstrap.py
+++ worker/bootstrap.py
@@ -4,21 +4,29 @@ import json
 
 from utils.io import oprint, eprint
 from utils.path import file_exists
+from utils.file import json_from_file
+
 from registry.check_geofile import REGISTRY
 
 
 def main():
     exist = file_exists(sys.argv[1])
-    if exist:
-        with open(sys.argv[1], 'r') as f:
-            try:
-                checks = sys.argv[2].split()
-                for check in checks:
-                    REGISTRY[check](sys.argv[1])
-            except Exception as e:
-                eprint("cannot convert args to json object :", str(e), sys.argv[2])
-            print("OPENED")
 
+    if exist:
+        geojson = json_from_file(sys.argv[1])
+        if geojson :
+            # with open(geojson, 'r') as f:
+            #     try:
+            #         checks = sys.argv[2].split()
+            #         for check in checks:
+            #             REGISTRY[check](sys.argv[1])
+            #     except Exception as e:
+            #         eprint("cannot convert args to json object :", str(e), sys.argv[2])
+            print("OPENED")
+        else :
+            raise FileNotFoundError("cannot convert file to geojson : {}".format(sys.argv[1]))
+    else :
+        raise FileNotFoundError("cannot find input file : {}".format(sys.argv[1]))
 
 if __name__ == "__main__":
     main()
--- worker/utils/file.py
+++ worker/utils/file.py
@@ -1,3 +1,27 @@
-def check_key_pair(jsonfile):
-    # return (k, v) in dict_to_check.viewitems()
+import json
+from utils.geometry import dxf_to_geojson
+def check_key_pair(k, v, dict_to_check):
+    return (k, v) in dict_to_check.viewitems()
+
+def border_exists(geojson_dict) :
+
     return True
+
+def dict_from_json(jsonfile) :
+    try :
+        converted_json = json.loads(jsonfile)
+        return converted_json
+    except Exception as e:
+        print(str(e))
+    return False
+
+def json_from_file(filepath) :
+    if filepath.lower().endswith(('.json')) :
+        return filepath
+    if filepath.lower().endswith(('.dxf')) :
+        try :
+            dxf = dxf_to_geojson(filepath)
+            return dxf
+        except Exception as e :
+            print(str(e))
+            return False
